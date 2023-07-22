301d48bf35efca776d019906bd240ea185a5a27c
Krisz6 <szalay.krisztian93@gmail.com>
2023-07-22 14:39:11 +0200

    Final version

---
+++ src/person/Gender.java
@@ -0,0 +1,21 @@
+public enum Gender {
+    MALE("male", "férfi"),
+    FEMALE("female", "nő");
+
+    private final String english;
+    private final String hungarian;
+
+    Gender(String english, String hungarian) {
+        this.english = english;
+        this.hungarian = hungarian;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public String getHungarian() {
+        return hungarian;
+    }
+}
+
